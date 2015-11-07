832af4b160b9fc1cfaf17659572905a75714a53d
John Ericson <Ericson2314@Yahoo.com>
2015-11-07 14:30:38 -0800

    fetchgitLocal: Revert to using current index
    When using nix in multi-user mode, the build users will not have permission
    to modify the user's repo (nor should they). Thus my trick of backing up the
    index and then running `git add .` will not work.

--- pkgs/build-support/fetchgitlocal/default.nix
+++ pkgs/build-support/fetchgitlocal/default.nix
@@ -3,24 +3,15 @@
 let
   srcStr = toString src;
 
-  # Adds the current directory (respecting ignored files) to the git store, and returns the hash
+  # Adds the current directory in the index (respecting ignored files) to the git store,
+  # and returns the hash
   gitHashFile = runCommand "put-in-git" {
       nativeBuildInputs = [ git ];
       dummy = builtins.currentTime; # impure, do every time
       preferLocalBuild = true;
     } ''
-      cd ${srcStr}
-      DOT_GIT=$(git rev-parse --resolve-git-dir .git) # path to repo
-
-      cp $DOT_GIT/index $DOT_GIT/index-user # backup index
-      git reset # reset index
-      git add . # add current directory
-
-      # hash of current directory
-      # remove trailing newline
-      git write-tree | tr -d '\n' > $out
-
-      mv $DOT_GIT/index-user $DOT_GIT/index # restore index
+      # `tr` to remove trailing newline
+      git -C ${srcStr} write-tree | tr -d '\n' > $out
     '';
 
   gitHash = builtins.readFile gitHashFile; # cache against git hash
