632a44b43e89b209d71734decbf9e6d5b9a88987
Matthieu Coudron <mattator@gmail.com>
2017-11-23 08:11:09 +0900

    now works

--- pkgs/build-support/fetchgitlocal/default.nix
+++ pkgs/build-support/fetchgitlocal/default.nix
@@ -13,20 +13,14 @@ let
       dummy = currentTime;
       preferLocalBuild = true;
     } ''
-      # cd ${srcStr}
-      set -x sh
-      echo CWD=$PWD
-
-      # `tr` to remove trailing newline
+      # we need write access to update index
       cp -r ${srcStr}/.git ${tmpFolder}
       chmod a+w ${tmpFolder}
       export GIT_DIR=${tmpFolder}
 
-      # > $out
-      echo OUT=$out
-      echo USER=$USER
+      # `tr` to remove trailing newline
       res="$(git rev-parse --show-prefix)"
-      git write-tree --prefix="$res" | tr -d '\n' >  $out
+      git write-tree --prefix="$res" | tr -d '\n' > $out
     '';
 
   gitHash = builtins.readFile gitHashFile; # cache against git hash
@@ -35,19 +29,15 @@ let
       nativeBuildInputs = [ git ];
       preferLocalBuild = true;
     } ''
-      set -x
       mkdir $out
 
       # git annoyingly breaks without doing this since the hash does
       # not correspond to repo root.
-      # -C => as if git was run from tthat place
-      # show-top-level => sjow fullpath
       cd $(git -C ${srcStr} rev-parse --show-toplevel)
 
       # dump tar of *current directory* at given revision
       hash="${gitHash}"
-      git archive --format=tar $hash \
-        | tar xv - -C $out
+      git archive --format=tar $hash | tar xv -C $out
     '';
 
 in nixPath
