783c651dd07eaaa59c3e1e25f4d15d83647fd4d6
Matthieu Coudron <mattator@gmail.com>
2017-12-13 16:01:50 +0900

    commit

--- pkgs/top-level/perl-packages.nix
+++ pkgs/top-level/perl-packages.nix
@@ -1,5 +1,4 @@
-/* This file definndInputs "$i" nativePkgs propagated-native-build-inputs
-donees the composition for CPAN (Perl) packages.  It has
+/* This file defines the composition for CPAN (Perl) packages.  It has
    been factored out of all-packages.nix because there are so many of
    them.  Also, because most Nix expressions for CPAN packages are
    trivial, most are actually defined here.  I.e. there's no function
