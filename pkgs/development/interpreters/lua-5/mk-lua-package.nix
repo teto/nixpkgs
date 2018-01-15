90e02ebeab05a4bba01fb386fdde28d2320766a1
Matthieu Coudron <mattator@gmail.com>
2018-01-16 08:28:04 +0900

    compiles

--- pkgs/development/interpreters/lua-5/mk-lua-package.nix
+++ pkgs/development/interpreters/lua-5/mk-lua-package.nix
@@ -7,23 +7,20 @@
 , wrapLua
 , unzip
 , writeText
-
-# Whether the derivation provides a Python module or not.
+# Whether the derivation provides a lua module or not.
 , toLuaModule
-
-# adds a postUnpackHooks (can we discard ?)
-, ensureNewerSourcesHook
 }:
 
 {
 name ? "${attrs.pname}-${attrs.version}"
 
 , version
-# by default prefix `name` e.g. "python3.3-${name}"
+
+# by default prefix `name` e.g. "lua5.2-${name}"
 , namePrefix ? lua.name + "-"
 
 # Dependencies for building the package
-, buildInputs ? [  ]
+, buildInputs ? []
 
 # Dependencies needed for running the checkPhase.
 # These are added to buildInputs when doCheck = true.
@@ -34,10 +31,7 @@ name ? "${attrs.pname}-${attrs.version}"
 , propagatedBuildInputs ? []
 , propagatedNativeBuildInputs ? []
 
-# DEPRECATED: use propagatedBuildInputs
-# , pythonPath ? []
-
-# used to disable derivation, useful for specific python versions
+# used to disable derivation, useful for specific lua versions
 , disabled ? false
 
 # Raise an error if two packages are installed with the same name
@@ -62,52 +56,30 @@ name ? "${attrs.pname}-${attrs.version}"
 
 # Keep extra attributes from `attrs`, e.g., `patchPhase', etc.
 if disabled
-# .executable
 then throw "${name} not supported for interpreter ${lua}"
 else
 
 let
-
   # todo use this
-  # Used to be $PWD
   luarocks_config = writeText "luarocksConfig" ''
     local_cache = ""
-    '';
+  '';
 in
-
-# python now does:
-# toPythonModule (python.stdenv.mkDerivation (builtins.removeAttrs attrs [
-#     "disabled" "checkInputs" "doCheck" "doInstallCheck" "dontWrapPythonPrograms" "catchConflicts"
-#   ] // {
 toLuaModule ( lua.stdenv.mkDerivation (
 builtins.removeAttrs attrs ["disabled" "checkInputs"] // {
 
   name = namePrefix + name;
 
-
   buildInputs = [ wrapLua luarocks ]
-    # ++ [ (ensureNewerSourcesHook { year = "1980"; }) ]
     ++ buildInputs
-    # might get rid of ?
     ++ lib.optionals doCheck checkInputs
     ;
 
-  # propagate python/setuptools to active setup-hook in nix-shell
-  # propagatedBuildInputs = propagatedBuildInputs ++ [ lua ];
+  # propagate lua to active setup-hook in nix-shell
   propagatedBuildInputs = propagatedBuildInputs ++ [ lua ];
-  # Python packages don't have a checkPhase, only an installCheckPhase
   doCheck = false;
-  doInstallCheck = doCheck;
 
-
-  # luarocks_cfg = "$(pwd)/luarocks_cfg";
-  #   '';
-  # LUAROCKS_CONFIG="${luarocks_cfg}";
-  #   output=$(luarocks unpack --verbose --force "$renamed")
-  #   '';
-
-  # that works only for src.rock !
-  # stripVersion
+  # that works only for src.rock
   setSourceRoot= let
     name_only=(builtins.parseDrvName name).name;
     in ''
@@ -118,21 +90,19 @@ builtins.removeAttrs attrs ["disabled" "checkInputs"] // {
 
   configurePhase = ''
     echo "Skipping configurePhase as luarocks is in charge"
-    '';
+  '';
 
-  # TODO fix hooks, run them etc..
-  # preBuild
   buildPhase = ''
     runHook preBuild
-    echo "we are in folder $PWD"
     export LUAROCKS_CONFIG="$PWD/luarocks_cfg"
     echo "local_cache = '$PWD'" > "$LUAROCKS_CONFIG"
 
 
     # TODO record in nix package the type of luarocks package it is, if it's make based,
     # one might want to patch shebangs for instance
-    echo "Patching shebangs"
     patchShebangs .
+
+    runHook postBuild
   '';
 
 
@@ -140,20 +110,6 @@ builtins.removeAttrs attrs ["disabled" "checkInputs"] // {
   postFixup = lib.optionalString (!dontWrapLuaPrograms) ''
     wrapLuaPrograms
   '' + attrs.postFixup or '''';
-
-    # export PYTHONPATH="$out/${python.sitePackages}:$PYTHONPATH"
-
-  # posthook run for the current derivation only
-  # postHook = ''
-  #   # function addLuaPath() {
-  #   echo "running the hook dude"
-  #   folder="$out/lib/lua/${lua.libFolder}"
-  #         export LUA_PATH="$folder:$LUA_PATH"
-  #         export LUA_CPATH="$folder:$LUA_CPATH"
-  #     # }
-  #   # envHooks+=(addLuaPath)
-  #   '';
-  # TODO maybe we can remove the unpackPhase as it will be redownloaded
 
 
   # inspired from build-python-setup-tools
@@ -166,70 +122,22 @@ builtins.removeAttrs attrs ["disabled" "checkInputs"] // {
     ${postShellHook}
   '';
 
-  # count on luarocks to install it
-    # luarocks install --tree $out ${name}
   installPhase = attrs.installPhase or ''
-
-    echo "STARTED INSTALL"
     runHook preInstall
 
-
-    echo "Looking for the folder fron $PWD"
-    # set -x
-    # TODO set it as $sourceRoot
-
-    # make assumes sources are available in cwd
-    # $rockspec
+    # luarocks make assumes sources are available in cwd
     # After the build is complete, it also installs the rock.
     # If no argument is given, it looks for a rockspec in the current directory
     # one problem here is that luarocks install packages in subfolders
     # so we patch luarocks !
     luarocks make --deps-mode=none --verbose --tree $out
-    # folder=$(find . -mindepth 2 -maxdepth 2 -type d -path '*$}*'|head -n1)
 
-    # to prevent collision when creating the environment
-    # might be possible to prevent htat with a better default config for luarocks
+    # to prevent collisions when creating environments
     # also added -f as it doesn't always exist
-# building path(s) ‘/nix/store/jc5ln503d83z24jbbkw91c06dqr54l59-lua-5.2.3-env’
-# collision between `/nix/store/8vzwia4dynqf367psvi517bjsk3pfkys-lua-5.2.3-lua_cliargs-3.0-1/lib/luarocks/rocks-5.2/manifest' and `/nix/store/s24bdjjr506vdpk71isakl600ryg5yfa-lua-5.2.3-busted-2.0.rc12-1/lib/luarocks/rocks-5.2/manifest'
-
     rm -f $out/lib/luarocks/rocks-5.2/manifest
-    # install --deps-mode=none should work too
-
-  #   addToLuaSearchPath LUA_PATH "$out/lib/lua/${lua.luaversion}" "/?.lua"
-  #   addToLuaSearchPath LUA_PATH "$out/share/lua/${lua.luaversion}" "/?.lua"
-  #   addToLuaSearchPath LUA_CPATH "$out/lib/lua/${lua.luaversion}" "/?.so"
-  #   addToLuaSearchPath LUA_CPATH "$out/share/lua/${lua.luaversion}" "/?.so"
-
-  #   export LUA_PATH="from_install_toto:$LUA_PATH"
-  #   export LUA_CPATH="from_install_tata:$LUA_CPATH"
-
-  #   # /bin/pip install *.whl --no-index --prefix=$out --no-cache
 
     runHook postInstall
-
-  #   echo "finished install"
-  #   echo "LUA_PATH=$LUA_PATH"
-  #   echo "LUA_CPATH=$LUA_CPATH"
   '';
-
-  # installPhase = attrs.installPhase or ''
-  #   runHook preInstall
-
-  #   # mkdir -p "$out/${lua.libFolder}"
-  #   # can be 5.2 for instance
-  #   folder="$out/lib/lua/${lua.libFolder}"
-  #   echo "install ran"
-  #   export TOTO="YO MAN!"
-  #   echo "installPhase run"
-  #   echo "Checking for folder '$folder'"
-  #   # if [ -d "$folder" ]; then
-  #     # export LUA_PATH="toto:$LUA_PATH"
-  #     # export LUA_CPATH="$folder:$LUA_CPATH"
-  #   # fi
-
-  #   runHook postInstall
-  # '';
 
   passthru = {
     inherit lua; # The lua interpreter
@@ -241,7 +149,5 @@ builtins.removeAttrs attrs ["disabled" "checkInputs"] // {
   } // meta // {
     # add extra maintainer(s) to every package
     maintainers = (meta.maintainers or []) ++ [ ];
-    # a marker for release utilities to discover python packages
-    # isBuildLuaPackage = lua.meta.platforms;
   };
 }))
--- pkgs/development/interpreters/lua-5/wrapper.nix
+++ pkgs/development/interpreters/lua-5/wrapper.nix
@@ -7,49 +7,29 @@
 }:
 
 # Create a lua executable that knows about additional packages.
-# LUA_PATH should already be built at this point ?!!
 let
   env = let
-    # I removed recursivePthLoader  but check why
-    # closePropagation is in deprecated.nix
-    # stdenv.lib.closePropagation
     paths =  requiredLuaModules (extraLibs ++ [ lua ] );
-  # here it's supposed to symlink
   in buildEnv {
     name = "${lua.name}-env";
 
     inherit paths;
     inherit ignoreCollisions;
 
-    # c la qu'on doit avoir un hook no ?
     # we create wrapper for the binaries in the different packages
-      # echo "paths=${lib.concatList paths}"
     postBuild = ''
-      # LUA_PATH est nul la
-      # lib.catAttrs " "
 
-      # si la tu recharges le truc
       . "${makeWrapper}/nix-support/setup-hook"
+
+      # get access to lua functions
       . ${lua}/nix-support/setup-hook
-      # echo "postBuild wrapper"
 
       if [ -L "$out/bin" ]; then
           unlink "$out/bin"
       fi
       mkdir -p "$out/bin"
 
-
-      # TODO Fix
-      # @luaversion@
-      # addToLuaSearchPath
       addToLuaPath $out
-      # program_LUA_PATH="$out/lib/lua/5.2/?.lua;$out/share/lua/5.2/?.lua;$out/lib/lua/5.2/?/init.lua;$out/share/lua/5.2/?/init.lua"
-      # program_LUA_CPATH="$out/lib/lua/5.2/?.so;$out/share/lua/5.2/?.so"
-
-      # echo "program_LUA_PATH=$program_LUA_PATH"
-      # echo "program_LUA_CPATH=$program_LUA_CPATH"
-      echo "LUA_PATH=$LUA_PATH"
-      echo "LUA_CPATH=$LUA_CPATH"
 
       # take every binary from lua packages and put them into the env
       for path in ${stdenv.lib.concatStringsSep " " paths}; do
@@ -60,18 +40,6 @@ let
             if [ -f "$prg" ]; then
               rm -f "$out/bin/$prg"
               if [ -x "$prg" ]; then
-                # --set LUA_PATH "$out"
-                # todo use --PREFIX instead ?
-                # TODO add itself to LUA_PATH
-                echo "generating wrapper for $prg"
-      # set -x
-
-                # TODO fix this value is null there
-                # --set LUA_PATH "$LUA_PATH" --set LUA_CPATH "ZEP:$LUA_CPATH"
-                # use --prefix / suffix LUA_PATH $out/lib/version
-                # --suffix LUA_PATH $program_LUA_PATH  ";" --suffix LUA_CPATH $program_LUA_CPATH ";"
-                # ENV SEP VAL
-                # makeWrapper "$path/bin/$prg" "$out/bin/$prg" --suffix LUA_PATH ';' "$program_LUA_PATH"   --suffix LUA_CPATH ';' "$program_LUA_CPATH"
                 makeWrapper "$path/bin/$prg" "$out/bin/$prg" --suffix LUA_PATH ';' "$LUA_PATH"   --suffix LUA_CPATH ';' "$LUA_CPATH"
               fi
             fi
@@ -80,7 +48,6 @@ let
       done
     '' + postBuild;
 
-    # hum check what's the passthru thing
     inherit (lua) meta;
 
     passthru = lua.passthru // {
@@ -89,8 +56,6 @@ let
       env = stdenv.mkDerivation {
         name = "interactive-${lua.name}-environment";
         nativeBuildInputs = [ env ];
-
-        # setupHook = "";
 
         buildCommand = ''
           echo >&2 ""
