c52325eb69f0487170fc8c5c7762e2ff3d6ce9b5
Matthieu Coudron <mattator@gmail.com>
2019-04-14 16:27:14 +0900

    ns-3: 28 -> 29
    renamed generateBinding to the more conventional pythonSupport.
    This fixed a few issues in binding generation but they are not yet operational.
    Even upstream has some problems with it.

--- pkgs/development/libraries/science/networking/ns3/default.nix
+++ pkgs/development/libraries/science/networking/ns3/default.nix
@@ -22,7 +22,7 @@
 , dia, tetex ? null, ghostscript ? null, texlive ? null
 
 # generates python bindings
-, generateBindings ? false, ncurses ? null
+, pythonSupport ? false, ncurses ? null
 
 # All modules can be enabled by choosing 'all_modules'.
 # we include here the DCE mandatory ones
@@ -34,13 +34,13 @@
 let
   pythonEnv = python.withPackages(ps:
     stdenv.lib.optional withManual ps.sphinx
-    ++ stdenv.lib.optionals generateBindings (with ps;[ pybindgen pygccxml ])
+    ++ stdenv.lib.optionals pythonSupport (with ps;[ pybindgen pygccxml ])
   );
 in
 stdenv.mkDerivation rec {
 
   name = "ns-3.${version}";
-  version = "28";
+  version = "29";
 
   # the all in one https://www.nsnam.org/release/ns-allinone-3.27.tar.bz2;
   # fetches everything (netanim, etc), this package focuses on ns3-core
@@ -48,27 +48,34 @@ stdenv.mkDerivation rec {
     owner  = "nsnam";
     repo   = "ns-3-dev-git";
     rev    = name;
-    sha256 = "17kzfjpgw2mvyx1c9bxccnvw67jpk09fxmcnlkqx9xisk10qnhng";
+    sha256 = "17h5qm9y0qp24ggajyz4shck3ngrc1j5c3amcimz2kfhqw5k50xs";
   };
 
   nativeBuildInputs = [ wafHook ];
+
+  outputs = [ "out" ] ++ lib.optional pythonSupport "py";
+
   # ncurses is a hidden dependency of waf when checking python
-  buildInputs = lib.optionals generateBindings [ castxml ncurses ]
-    ++ stdenv.lib.optional enableDoxygen [ doxygen graphviz imagemagick ]
-    ++ stdenv.lib.optional withManual [ dia tetex ghostscript texlive.combined.scheme-medium ];
+  buildInputs = lib.optionals pythonSupport [ castxml ncurses ]
+    ++ lib.optional enableDoxygen [ doxygen graphviz imagemagick ]
+    ++ lib.optional withManual [ dia tetex ghostscript texlive.combined.scheme-medium ];
 
   propagatedBuildInputs = [ gcc6 pythonEnv ];
 
+  # patchShebangs ./waf
   postPatch = ''
     patchShebangs doc/ns3_html_theme/get_version.sh
   '';
+
+  # configureScript = "${pythonEnv.interpreter} ./waf configure";
 
   configureFlags = with stdenv.lib; [
       "--enable-modules=${stdenv.lib.concatStringsSep "," modules}"
       "--with-python=${pythonEnv.interpreter}"
   ]
   ++ optional (build_profile != null) "--build-profile=${build_profile}"
-  ++ optional generateBindings [  ]
+  # maybe do it afterwards ? not needed , it's done automatically
+  # ++ optional pythonSupport "--apiscan=all"
   ++ optional withExamples " --enable-examples "
   ++ optional doCheck " --enable-tests "
   ;
@@ -77,7 +84,26 @@ stdenv.mkDerivation rec {
     + lib.optionalString enableDoxygen " doxygen"
     + lib.optionalString withManual "sphinx";
 
+  # to prevent fatal error: 'backward_warning.h' file not found
+  CXXFLAGS = "-D_GLIBCXX_PERMIT_BACKWARD_HASH";
+
+  postBuild = with stdenv.lib; let flags = concatStringsSep ";" (
+      optional enableDoxygen "./waf doxygen"
+      ++ optional withManual "./waf sphinx"
+    );
+    in "${flags}"
+  ;
+
   doCheck = true;
+
+  postInstall = ''
+    set -x
+    echo $PWD
+    moveToOutput "${pythonEnv.libPrefix}" "$py"
+    set +x
+
+    # mv "${pythonEnv.libPrefix}" "$py"
+  '';
 
   # we need to specify the proper interpreter else ns3 can check against a
   # different version even though we
@@ -85,7 +111,8 @@ stdenv.mkDerivation rec {
     ${pythonEnv.interpreter} ./test.py
   '';
 
-  hardeningDisable = [ "fortify" ];
+  # strictoverflow prevents clang from discovering pyembed when bindings
+  hardeningDisable = [ "fortify" "strictoverflow"];
 
   meta = {
     homepage = http://www.nsnam.org;
