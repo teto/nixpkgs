15404f6e20af72e710bdadfbf9915968336fead9
Matthieu Coudron <886074+teto@users.noreply.github.com>
2024-03-06 22:40:01 +0100

    lua.tests: update

--- pkgs/development/interpreters/lua-5/tests/assert.sh
+++ pkgs/development/interpreters/lua-5/tests/assert.sh
@@ -3,19 +3,19 @@
 # Example:
 #     fail "It should have been but it wasn't to be"
 function fail() {
-    echo "$1"
+    echo -e "$1"
     exit 1
 }
 
 
 function assertStringEqual() {
     if ! diff <(echo "$1") <(echo "$2") ; then
-        fail "expected \"$1\" to be equal to \"$2\""
+        fail "expecting \"$1\"\nto equal the reference value \"$2\""
     fi
 }
 
 function assertStringContains() {
     if ! echo "$1" | grep -q "$2" ; then
-        fail "expected \"$1\" to contain \"$2\""
+        fail "expecting:\n\"$1\"\nto contain the reference value:\n\"$2\""
     fi
 }
--- pkgs/development/interpreters/lua-5/tests/default.nix
+++ pkgs/development/interpreters/lua-5/tests/default.nix
@@ -1,12 +1,9 @@
 { lua
 , hello
 , wrapLua
-, lib, fetchFromGitHub
-, fetchFromGitLab
 , pkgs
 }:
 let
-
   runTest = lua: { name, command }:
     pkgs.runCommandLocal "test-${lua.name}-${name}" ({
       nativeBuildInputs = [lua];
@@ -18,29 +15,42 @@ let
     + "touch $out"
     );
 
-    wrappedHello = hello.overrideAttrs(oa: {
-      propagatedBuildInputs = [
-        wrapLua
-        lua.pkgs.cjson
-      ];
-      postFixup = ''
-        wrapLuaPrograms
-      '';
-    });
+  wrappedHello = hello.overrideAttrs(oa: {
+    propagatedBuildInputs = [
+      wrapLua
+      lua.pkgs.cjson
+    ];
+    postFixup = ''
+      wrapLuaPrograms
+    '';
+  });
 
-    luaWithModule = lua.withPackages(ps: [
-      ps.lua-cjson
-    ]);
+  luaWithModule = lua.withPackages(ps: [
+    ps.lua-cjson
+  ]);
+
+  golden_LUA_PATHS = {
+
+    "5.1" = "./?.lua;${lua}/share/lua/5.1/?.lua;${lua}/share/lua/5.1/?/init.lua;${lua}/lib/lua/5.1/?.lua;${lua}/lib/lua/5.1/?/init.lua";
+    "5.2" = "${lua}/share/lua/5.2/?.lua;${lua}/share/lua/5.2/?/init.lua;${lua}/lib/lua/5.2/?.lua;${lua}/lib/lua/5.2/?/init.lua;./?.lua";
+    "5.3" = "${lua}/share/lua/5.3/?.lua;${lua}/share/lua/5.3/?/init.lua;${lua}/lib/lua/5.3/?.lua;${lua}/lib/lua/5.3/?/init.lua;./?.lua;./?/init.lua";
+    "5.4" = "${lua}/share/lua/5.4/?.lua;${lua}/share/lua/5.4/?/init.lua;${lua}/lib/lua/5.4/?.lua;${lua}/lib/lua/5.4/?/init.lua;./?.lua;./?/init.lua";
+  };
 in
   pkgs.recurseIntoAttrs ({
 
-  checkAliases = runTest lua {
-    name = "check-aliases";
+  checkInterpreterPatch = let
+    golden_LUA_PATH = golden_LUA_PATHS.${lua.luaversion};
+  in
+    runTest lua {
+    name = "check-default-lua-path";
     command = ''
       generated=$(lua -e 'print(package.path)')
-      golden_LUA_PATH='./share/lua/${lua.luaversion}/?.lua;./?.lua;./?/init.lua'
-
-      assertStringContains "$generated" "$golden_LUA_PATH"
+      echo "lua: ${lua}"
+      echo "${golden_LUA_PATH}"
+      assertStringEqual "$generated" "${golden_LUA_PATH}"
+      export LUA_PATH=";;"
+      assertStringEqual "$generated" "${golden_LUA_PATH}"
       '';
   };
 
