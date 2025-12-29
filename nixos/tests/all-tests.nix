8b9b116b379a71f5dfd4e5d071ef23935b23d7f0
Richard Palethorpe <io@richiejp.com>
2025-12-29 10:11:13 +0000

    nixos/voxinput: init at 1.0.0

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -1944,6 +1944,7 @@ in
   virtualbox = handleTestOn [ "x86_64-linux" ] ./virtualbox.nix { };
   vm-variant = handleTest ./vm-variant.nix { };
   vnstat = runTest ./vnstat.nix;
+  voxinput = runTest ./voxinput.nix;
   vscode-remote-ssh = handleTestOn [ "x86_64-linux" ] ./vscode-remote-ssh.nix { };
   vscodium = import ./vscodium.nix { inherit runTest; };
   vsftpd = runTest ./vsftpd.nix;
