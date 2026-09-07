87fbf6213a5815eef989d3b655b01a88e7b0cfa2
teto <886074+teto@users.noreply.github.com>
2026-09-08 01:24:36 +0200

    fix upmpdcli

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -6675,11 +6675,6 @@ with pkgs;
 
   unixodbcDrivers = recurseIntoAttrs (callPackages ../development/libraries/unixODBCDrivers { });
 
-  upmpdcli = callPackage ../tools/networking/upmpdcli {
-    python = python3;
-  };
-
-
   valeStyles = recurseIntoAttrs (callPackages ../by-name/va/vale/styles.nix { });
 
   vencord-web-extension = callPackage ../by-name/ve/vencord/package.nix { buildWebExtension = true; };
